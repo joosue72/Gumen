b65cbd309585ecae13077bb081a6e1e9b4cceec1
jose mena <josemanuelsalas10@hotmail.com>
2020-06-05 19:41:05 -0500

    version 1.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+
+
 import 'package:flutter/material.dart';
+import 'package:gumen/Animation/FadeAnimation.dart';
+
 import 'package:splashscreen/splashscreen.dart';
+
+
 
 void main() {
   runApp(new MaterialApp(
   debugShowCheckedModeBanner: false,
-  home: new MyApp(),
+  home: Login(),
 ));
 }
 
@@ -18,11 +24,11 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return SplashScreen(
-      seconds: 15,
+      seconds: 5,
       backgroundColor: Colors.black,
       image: Image.asset('assets/21362-animation.gif'),
       loaderColor: Colors.white,
-      photoSize: 15.0,
+      photoSize: 5.0,
       navigateAfterSeconds: MainScreen(),
       
     );
@@ -48,3 +54,138 @@ class MainScreen extends StatelessWidget {
     );
   }
 }
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              height: 400,
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    top: -40,
+                    height: 400,
+                    width: width,
+                    child: FadeAnimation(1, Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/background.png'),
+                          fit: BoxFit.fill
+                        )
+                      ),
+                    )),
+                  ),
+                  Positioned(
+                    height: 400,
+                    width: width+20,
+                    child: FadeAnimation(1.3, Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/background-2.png'),
+                          fit: BoxFit.fill
+                        )
+                      ),
+                    )),
+                  ),
+                  Positioned(
+                    height: 300,
+                    width: width+10,
+                    child: FadeAnimation(1.3, Container(
+                      decoration: BoxDecoration(
+                        
+                        image: DecorationImage(
+                          
+                          image: AssetImage('assets/logogumen.png')
+                          
+                        )
+                      ),
+                    )),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  FadeAnimation(1.5, Text("Login", style: TextStyle(color: Color.fromRGBO(49, 39, 79, 1), fontWeight: FontWeight.bold, fontSize: 30),)),
+                  SizedBox(height: 30,),
+                  FadeAnimation(1.7, Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color.fromRGBO(196, 135, 198, .3),
+                          blurRadius: 20,
+                          offset: Offset(0, 10),
+                        )
+                      ]
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                  
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Password",
+                              hintStyle: TextStyle(color: Colors.grey)
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  )),
+                  SizedBox(height: 20,),
+                  
+                  FadeAnimation(1.9, Container(
+                    height: 50,
+                    margin: EdgeInsets.symmetric(horizontal: 60),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Color.fromRGBO(49, 39, 79, 1),
+                    ),
+                    child: Center(
+                      child: Text("Login", style: TextStyle(color: Colors.white),),
+                    ),
+                  )),
+                  
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+
+
