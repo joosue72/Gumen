176f05a71adc2eb5e958e02fd8fd65ccc6726536
jose mena <josemanuelsalas10@hotmail.com>
2020-06-26 14:35:43 -0500

    Version 1.0.3

--- lib/Ventas.dart
+++ lib/Ventas.dart
@@ -20,6 +20,7 @@ double costo;
 String producto;
 DateTime now = DateTime.now();
 String fecha = DateFormat('kk:mm:ss \n EEE d MMM').format(now);
+String mes = DateFormat('MMM').format(now);
 String nombres;
 var selectedCurrency, selectedType;
 
@@ -247,7 +248,7 @@ class _VentasState extends State<Ventas> {
                       );
                     }
                   }),
-                  SizedBox(width: 20.0,),
+                  SizedBox(width: 10.0,),
                    Container(
         height: 40.0,
         width: 40.0,
@@ -412,7 +413,49 @@ class _VentasState extends State<Ventas> {
       _formKey4.currentState.save();
       _formKey5.currentState.save();
       String fecha = DateFormat('kk:mm:ss \n EEE d MMM').format(now);
-      DocumentReference ref = await db.collection('Ventas').add({'Nombre': '$nombre', 'Cantidad': '$cantidad', 'Costo': '$costo', 'Fecha': '$fecha','Producto': '$selectedCurrency', 'pendiente': '$pendiente'});
+      String mes = DateFormat('MMM').format(now);
+      int numerofecha;
+
+      switch(mes)
+      {
+        case 'Jan':
+            numerofecha = 1;
+        break;
+        case 'Feb':
+            numerofecha = 2;
+        break;
+        case 'Mar':
+            numerofecha = 3;
+        break;
+        case 'Apr':
+            numerofecha = 4;
+        break;
+        case 'May':
+            numerofecha = 5;
+        break;
+        case 'Jan':
+            numerofecha = 6;
+        break;
+        case 'Jul':
+            numerofecha = 7;
+        break;
+        case 'Aug':
+            numerofecha = 8;
+        break;
+        case 'Sep':
+            numerofecha = 9;
+        break;
+        case 'Oct':
+            numerofecha = 10;
+        break;
+        case 'Nov':
+            numerofecha = 11;
+        break;
+        case 'Dec':
+            numerofecha = 12;
+        break;
+      }
+      DocumentReference ref = await db.collection('Ventas').add({'Nombre': '$nombre', 'Cantidad': '$cantidad', 'Costo': '$costo', 'Fecha': '$fecha','Producto': '$selectedCurrency', 'pendiente': '$pendiente', 'Mes': '$numerofecha'});
       setState(() => id = ref.documentID);
       print(ref.documentID);
     }
